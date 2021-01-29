2ea26115ab3882bbb490a070f041458259eee79f
Bmeimei <skystar505836915@gmail.com>
2021-01-29 13:08:59 -0800

    refactor F.A.M. with its parent class

--- Assignments/Assignment1/family_appointed_moderator.py
+++ Assignments/Assignment1/family_appointed_moderator.py
@@ -5,9 +5,6 @@
 
 from menu import ViewMenu
 from features import Features
-from user import User
-from user_types import UserTypes
-from budget import Budgets
 from categories import Categories
 
 
@@ -31,61 +28,6 @@ class FAM(ViewMenu, Features):
     - execute_features
 
     """
-
-    __categories_dict = {"1": Categories.GAMES_ENTERTAINMENT, "2": Categories.CLOTHING_ACCESSORISE,
-                         "3": Categories.EATING_OUT, "4": Categories.MISCELLANEOUS}
-    """
-    The Categories Dictionary.
-    """
-
-    __user_type_dict = {"1": UserTypes.ANGEL, "2": UserTypes.TROUBLEMAKER, "3": UserTypes.REBEL}
-    """
-    The User Type Dictionary.
-    """
-
-    def __init__(self) -> None:
-        """
-        Constructs a family appointed moderator.
-        Instantiates the default user, and the transaction record is an empty list for containing transactions.
-        """
-        self.__user = User()
-
-    def _registering_user(self) -> None:
-        """
-        The user (usually a parent) must register their child's financial details.
-        This includes (but is not necessarily limited to):
-
-        • The user name
-        • Age
-        • User Type
-        • Bank Account number
-        • Bank Name (optional)
-        • Bank Balance
-        • Their budgets
-        """
-        user_name = input("Please input user's name:")
-        age = int(input("Please input user's age:"))
-        user_type = self.__select_user_type()
-        budgets = Budgets()
-        bank_balance = float(input("Please input a positive bank balance:"))
-        bank_name = input("Please input the bank name (Optional):")
-        self.__user = User(user_name, age, user_type, budgets, bank_balance, bank_name)
-
-    def _assigning_budget_categories(self) -> None:
-        """
-        Each child that is being monitored is assigned the following budget categories.
-        The exact value of each budget is assigned when registering the child as a user.
-
-        • Games and Entertainment
-        • Clothing and Accessories
-        • Eating Out
-        • Miscellaneous
-        """
-        games_entertainment = float(input("Please input a positive budget for Games and Entertainment:"))
-        clothing_accessories = float(input("Please input a positive budget for Clothing and Accessories:"))
-        eating_out = float(input("Please input a positive budget for Eating Out:"))
-        miscellaneous = float(input("Please input a positive budget for miscellaneous:"))
-        self.__user.budgets = Budgets(games_entertainment, clothing_accessories, eating_out, miscellaneous)
 
     @staticmethod
     def _showing_menu() -> None:
@@ -139,8 +81,8 @@ class FAM(ViewMenu, Features):
                 option = input("Please type the menu command:")
                 self._processing_menu_option(option)
                 print()
-        # except TypeError:
-        #     print("Invalid Type!")
+        except TypeError:
+            print("Invalid Type!")
         except ValueError:
             print("Invalid Input Value!")
 
@@ -149,7 +91,7 @@ class FAM(ViewMenu, Features):
         Shows the user the current status of their budgets (locked or not)
         in addition to the amount spent, amount left, and the total amount allocated to the budget.
         """
-        print(self.__user.budgets)
+        print(self._user.budgets)
 
     def _record_transaction(self) -> None:
         """
@@ -160,7 +102,7 @@ class FAM(ViewMenu, Features):
         amount = float(input("Please type the positive amount for the purchase:"))
         category = self.__input_category()
         name = input("Please type the name of the shop/website where the purchase took place:")
-        self.__user.process_and_record_transaction(amount, category, name)
+        self._user.process_and_record_transaction(amount, category, name)
 
     def _view_transactions_by_budget(self) -> None:
         """
@@ -172,11 +114,11 @@ class FAM(ViewMenu, Features):
                        "2: Clothing and Accessorise\n"
                        "3: Eating Out\n"
                        "4: Miscellaneous\n")
-        if select not in self.__categories_dict:
+        if select not in self._categories_dict:
             print("Invalid Categories Command!")
             return None
-        category = self.__categories_dict[select]
-        transaction_record = self.__user.budgets.get_transaction_history()
+        category = self._categories_dict[select]
+        transaction_record = self._user.budgets.get_transaction_history()
         transaction_sublist = filter(lambda x: x.get_category_type() == category, transaction_record)
         transaction_sublist = sorted(transaction_sublist, key=lambda x: x.get_timestamp())
         if len(transaction_sublist) == 0:
@@ -189,8 +131,8 @@ class FAM(ViewMenu, Features):
         Prints out the bank account details of the user and all transactions
         conducted to date alongside the closing balance.
         """
-        print(self.__user)
-        transaction_record = self.__user.budgets.get_transaction_history()
+        print(self._user)
+        transaction_record = self._user.budgets.get_transaction_history()
         transaction_sorted_list = sorted(transaction_record, key=lambda x: x.get_timestamp())
         if len(transaction_sorted_list) == 0:
             print("Currently no transactions in this account.")
@@ -201,14 +143,16 @@ class FAM(ViewMenu, Features):
         """
         Exits the program and shows the user status.
         """
-        print(self.__user)
+        print(self._user)
+        print("-------------------------------\n"
+              "THANKS FOR USING F.A.M. ^ ^")
 
     def __input_category(self) -> Categories:
         """
         Helper function to select categories.
         :return: A specific category
         """
-        categories_dict = self.__categories_dict
+        categories_dict = self._categories_dict
         while True:
             select = input("Please type the category:\n"
                            "1: Games and Entertainment\n"
@@ -218,19 +162,6 @@ class FAM(ViewMenu, Features):
             if select in categories_dict:
                 return categories_dict[select]
             print("Invalid Input!")
-
-    def __select_user_type(self) -> UserTypes:
-        """
-        Helper function to select user type
-        :return: A specific UserType
-        """
-        user_type_dict = self.__user_type_dict
-        while True:
-            select = input("User Type: 1: Angel  2: Troublemaker  3: Rebel:")
-            if select in user_type_dict:
-                return user_type_dict[select]
-            print("Invalid Command! Please Type again\n"
-                  "----------------------------------")
 
 
 def main():
